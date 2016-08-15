e2d158305b5e034b8f4a503a0ca9f3aac33b538e
heyishi <heyishi@126.com>
2016-08-15 17:57:47 +0800

    微信定时器查询accessToken
    微信定时器查询accessToken 技术不足未能完成

---
+++ wechat/sync_access_token/access_test.go
@@ -0,0 +1,14 @@
+package syncAccessToken
+
+import (
+	"fmt"
+	"testing"
+
+	http "github.com/astaxie/beego/httplib"
+)
+
+func TestGet(t *testing.T) {
+	a, e := http.Get("https://www.baidu.com").String()
+	fmt.Println(e)
+	fmt.Println(a)
+}
--- wechat/sync_access_token/access_token_server.go
+++ wechat/sync_access_token/access_token_server.go
@@ -1,16 +1,30 @@
 package syncAccessToken
 
 import (
+	"sync/atomic"
+	"time"
+	"unsafe"
 	"wisemancode/log"
 	"wisemancode/utils"
+
+	"github.com/astaxie/beego/httplib"
 )
 
-//AccessToken 微信校验串
-type AccessToken struct {
-	AccessTokenStr string //ACCESS_TOKEN 字符串
-	StartTime      string //获取时间
-	EndTime        string //失效时间
-	LiveTime       int    //存活时间 秒
+//AccessTokenServer 定时获取微信校验服务
+type AccessTokenServer struct {
+	AccessTokenStr       chan string       //ACCESS_TOKEN 字符串 解析后的信息
+	accessTokenResultStr chan accessResult //微信返回信息后解析结果
+	StartTime            string            //获取时间
+	EndTime              string            //失效时间
+	LiveTime             int               //存活时间 秒
+	Times                time.Duration     //时间间隔
+
+	tokenCache unsafe.Pointer //微信返回的信息指针
+
+}
+type accessResult struct {
+	accessTokenStrNew string
+	err               string
 }
 
 //AcessTokenURL 获取维系字符串URL
@@ -21,6 +35,10 @@ type AcessTokenURL struct {
 	grantType      string
 	accessTokenURL string
 }
+
+const (
+	times time.Duration = 2 * 60 * 60
+)
 
 var (
 	accessToken *AcessTokenURL
@@ -41,4 +59,45 @@ func newAcessTokenURL() (urlToken *AcessTokenURL) {
 	grantType := utils.GetWxConfig("grant_type")
 	accessTokenURL := url + "?grant_type=" + grantType + "&&appid=" + appid + "&secret=" + appsecret
 	return &AcessTokenURL{url: url, aPPID: appid, appsecret: appsecret, grantType: grantType, accessTokenURL: accessTokenURL}
+}
+func NewAccessTokenServer() (accessTokenServer *AccessTokenServer) {
+	accessTokenStr := make(chan string)
+	access := make(chan accessResult)
+	accessTokenServer = &AccessTokenServer{AccessTokenStr: accessTokenStr, accessTokenResultStr: access, Times: times}
+	go accessTokenServer.GetAccessTokenByTime(accessToken)
+	return
+}
+
+//GetAccessTokenByTime 启动定时器进行获取微信accessToken
+func (this *AccessTokenServer) GetAccessTokenByTime(url *AcessTokenURL) {
+	//启动定时器到微信服务器中进行获取accessToken
+	//	currentAccessToken := this.AccessTokenStr
+	//if()
+	//accessToken, error := httplib.Get(url.accessTokenURL).String()
+	//	if error == nil {
+	//		//错误处理
+	//		return
+	//	}
+
+}
+
+type accessTokenJson struct {
+	Token     string `json:"access_token"`
+	ExpiresIn int64  `json:"expires_in"`
+}
+
+//获取新的token 放到缓存中 别且返回 新的token
+func (this *AccessTokenServer) GetNewAccessTokenFromWeiXin(url *AcessTokenURL, currentAccess string) (token *accessTokenJson, err error) {
+	if p := (*accessTokenJson)(atomic.LoadPointer(&this.tokenCache)); p != nil && currentAccess != p.Token {
+		return p, nil
+	}
+	data, err := httplib.Get(url.accessTokenURL).String()
+	log.Logger.Info("获取微信信息：data=：" + data)
+	log.Logger.Info("获取微信信息：err=：", err.Error())
+	if err != nil {
+		//处理错误信息
+		atomic.StorePointer(&this.tokenCache, nil)
+		return
+	}
+	return
 }
