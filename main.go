ac4b892fd07b0f4fd59162a63a9aeba304fda6c0
heyishi <heyishi@126.com>
2016-08-25 18:16:54 +0800

    添加sql  支持  添加前端页面
    添加sql  支持  添加前端页面

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"wisemancode/log"
 	_ "wisemancode/routers"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 //"wisemancode/wechat"
-
+func init() {
+	log.Logger.Info("初始化数据库=======start============")
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	//orm.RegisterDataBase("default", "mysql", "root:root@tcp(192.168.224.128:3306)/cloudta?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", "root:root@tcp(127.0.0.1:3306)/wx?charset=utf8")
+	log.Logger.Info("初始化数据库=======end=================")
+}
 func main() {
 	//wechat.MainCallbacl()
 	beego.Run()
